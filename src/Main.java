94dcd265548ef1a4f85826d7f75a9de33f147534
Alberto <amozosk@gmail.com>
2025-02-13 23:59:37 +0100

    Wordle solver

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         Solver solver = new Solver();
-        while(true) {
-            System.out.print("Palabra: ");
-            String palabra = s.nextLine();
+        do {
+            String palabra = solver.recommendWord();
+            System.out.println("Next word: " + palabra);
             System.out.print("Resultado (b, m, r): "); //bien, mal, regular
             String resultado = s.nextLine();
             cambiarEstado(solver, resultado, palabra);
-            solver.printPossibleWords();
-        }
+        } while(true);
     }
 
     public static void cambiarEstado(Solver solver, String resutado, String palabra) {
@@ -20,6 +19,7 @@ public class Main {
                 solver.setCorrect(i, palabra.charAt(i));
             } else if (resutado.charAt(i) == 'm') {
                 solver.setIncorrect(i, palabra.charAt(i));
+                solver.setNotIncluded(palabra.charAt(i));
             } else if (resutado.charAt(i) == 'r') {
                 solver.setUnplaced(palabra.charAt(i));
                 solver.setIncorrect(i, palabra.charAt(i));
--- src/Solver.java
+++ src/Solver.java
@@ -20,7 +20,7 @@ public class Solver {
             words = new String[2309];
             Scanner s = new Scanner(new FileReader("words-en.txt"));
             int i = 0;
-            while(s.hasNext()) {
+            while (s.hasNext()) {
                 words[i] = s.nextLine();
                 i++;
             }
@@ -30,11 +30,11 @@ public class Solver {
 
         general = new BitSet[26];
         for (int i = 0; i < 26; i++) {
-            char letter = (char)('a' + i);
+            char letter = (char) ('a' + i);
             BitSet b = new BitSet(words.length);
             for (int j = 0; j < words.length; j++) {
                 String word = words[j];
-                if(word.indexOf(letter) != -1) {
+                if (word.indexOf(letter) != -1) {
                     b.set(j);
                 }
             }
@@ -46,7 +46,7 @@ public class Solver {
             for (int character = 0; character < 26; character++) {
                 BitSet b = new BitSet(words.length);
                 for (int word = 0; word < words.length; word++) {
-                    if(words[word].charAt(posLetter) == (char)('a' + character)) {
+                    if (words[word].charAt(posLetter) == (char) ('a' + character)) {
                         b.set(word);
                     }
                 }
@@ -68,6 +68,91 @@ public class Solver {
 
     public void setUnplaced(char letter) {
         possibleWords.and(general[letter - 'a']);
+    }
+
+    public void setNotIncluded(char letter) {
+        possibleWords.andNot(general[letter - 'a']);
+    }
+
+    /**
+     * Método auxiliar que, dada una palabra de intento y una solución,
+     * genera un código numérico (entre 0 y 3^5-1) que representa el feedback.
+     * Cada posición se codifica en base 3:
+     * 2 -> Letra en posición correcta (verde)
+     * 1 -> Letra presente en otra posición (amarillo)
+     * 0 -> Letra ausente (gris)
+     */
+    private int getPattern(String guess, String solution) {
+        int[] feedback = new int[5];
+        boolean[] usedSolution = new boolean[5];
+
+        // Primero: marcar letras correctas (verde)
+        for (int i = 0; i < 5; i++) {
+            if (guess.charAt(i) == solution.charAt(i)) {
+                feedback[i] = 2;
+                usedSolution[i] = true;
+            }
+        }
+
+        // Segundo: marcar letras presentes en otra posición (amarillo)
+        for (int i = 0; i < 5; i++) {
+            if (feedback[i] == 0) { // no es verde
+                for (int j = 0; j < 5; j++) {
+                    if (!usedSolution[j] && guess.charAt(i) == solution.charAt(j)) {
+                        feedback[i] = 1;
+                        usedSolution[j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Codificar el feedback en un único entero (base 3)
+        int code = 0;
+        for (int i = 0; i < 5; i++) {
+            code = code * 3 + feedback[i];
+        }
+        return code;
+    }
+
+    /**
+     * Recomienda la palabra que maximiza la entropía esperada, es decir,
+     * la que, en promedio, proporcionará mayor información para reducir el
+     * conjunto de palabras posibles.
+     */
+    public String recommendWord() {
+        double bestEntropy = -1;
+        String bestGuess = null;
+        int totalPossible = possibleWords.cardinality();
+
+        // Se consideran todas las palabras del diccionario como posibles intentos.
+        for (int i = 0; i < words.length; i++) {
+            if (possibleWords.get(i)) {
+                String guess = words[i];
+                Map<Integer, Integer> patternCounts = new HashMap<>();
+
+                // Simular feedback para cada posible solución
+                for (int solIndex = possibleWords.nextSetBit(0); solIndex >= 0; solIndex = possibleWords.nextSetBit(solIndex + 1)) {
+                    String solution = words[solIndex];
+                    int pattern = getPattern(guess, solution);
+                    patternCounts.put(pattern, patternCounts.getOrDefault(pattern, 0) + 1);
+                }
+
+                // Calcular la entropía para esta palabra candidata
+                double entropy = 0.0;
+                for (int count : patternCounts.values()) {
+                    double p = (double) count / totalPossible;
+                    entropy += -p * (Math.log(p) / Math.log(2)); // log en base 2
+                }
+
+                // Seleccionar la palabra con mayor entropía
+                if (entropy > bestEntropy) {
+                    bestEntropy = entropy;
+                    bestGuess = guess;
+                }
+            }
+        }
+        return bestGuess;
     }
 
     public void printPossibleWords() {
